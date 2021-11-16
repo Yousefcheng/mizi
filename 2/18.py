8367080bb70acb9a50d966edc08e103a2c986fe2
yongfucheng <yongfucheng@qq.com>
2021-11-16 22:14:07 +0800

    11.16

--- 2/18.py
+++ 2/18.py
@@ -1,15 +1,18 @@
 string = input().split()
 num_list = []
 output_list = []
-
+sum = 0
 
 for i in string:
     if not i.isalpha():
+    # if 1:    
         num_list.append(eval(i))
+        # num_list.append(i)
         output_list.append(eval(i))
+        # output_list.append(i)
     else:
-        output_list.append(i)
-sum = 0
+        output_list.append(str(i))
+
 for i in num_list:
     sum = sum+i
 
---
+++ 7/61.py
@@ -0,0 +1,45 @@
+from math import *
+'''
+    海伦公式求面积
+'''
+def heron(a,b,c):
+    p=(a+b+c)/2
+    S=(p*(p-a)*(p-b)*(p-c))**0.5
+    return S
+
+'''
+求各个边的边长,输入三个数组
+'''
+def norm(a,b,c):
+    ab=( (b[0]-a[0])**2-(b[1]-a[1])**2 ) ** 0.5
+    ac=( (a[0]-c[0])**2-(a[1]-c[1])**2 ) ** 0.5
+    bc=( (b[0]-c[0])**2-(b[1]-c[1])**2 ) ** 0.5
+
+    return ab,ac,bc
+    
+def get_res(a,b,S):
+    ab=( (b[0]-a[0])**2-(b[1]-a[1])**2 ) ** 0.5
+    res=S/ab/2
+    return res
+
+
+res_list=[]
+
+
+x=eval(input())
+y=eval(input())
+dot=eval(input())
+
+for i in range(3):
+    ab,ac,bc=norm(x[i],y[i],dot)
+    S=heron(ab,ac,bc)
+    res=get_res(x[i],y[i],S)
+    res_list.append(res)
+
+print(res_list)
+
+
+
+
+
+
